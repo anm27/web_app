6ed9ef2b1643ec48041f5242c958fd2df3dc0b00
anm222 <anmishra20@gmail.com>
2022-12-21 22:30:00 +0530

    Writerszone: small changes

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
 
 let settings = {
   dots: true,
@@ -24,11 +25,28 @@ const Slides = () => {
             src="https://www.thebalancemoney.com/thmb/hadTedIePLFb_eUh3jgeQo37WpA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Stocksy_woman-writing-laptop_476082-57ab432d3df78cf459975331.jpg"
             alt="Blog 1"
             className="h-128 w-full object-cover"
-            style={{ opacity: 0.9 }}
+            style={{ opacity: 7 }}
           />
-          <h1 className="absolute inset-x-0 top-4 text-white lg:text-6xl md:text-3xl sm:text-3xl xs:text-2xl font-bold">
-            Enhance your business with rich content
-          </h1>
+          <div className="absolute inset-x-0 top-10 ">
+            <div className="text-white lg:text-6xl md:text-3xl sm:text-3xl xs:text-2xl font-bold">
+              <h1>Enhance your business with rich content</h1>
+              <h4 className="mt-10">Content do..!</h4>
+            </div>
+            <div className="flex-row space-x-4 mt-32">
+              <Link
+                to="/"
+                className="py-4 border-none rounded-sm shadow px-10 bg-gray-500 text-white"
+              >
+                About us
+              </Link>
+              <Link
+                to="/"
+                className="py-4 border-none rounded-sm shadow px-10 bg-blue-500 text-white"
+              >
+                Get Quote
+              </Link>
+            </div>
+          </div>
         </div>
         <div className="relative text-center">
           <img
